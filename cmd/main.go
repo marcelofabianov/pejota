c3c97ed655c7f92cf21d162d7027baf7dbf697aa
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-13 11:21:37 -0300

    chore: adicionar viper.go para carregar configuracoes

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"log"
+
+	"github.com/marcelofabianov/pejota.git/config"
+)
+
 func main() {
-	//...
+	if err := config.Load(); err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
 }
