5b76647401b81851c407d29db9214eb2dd622d5f
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-13 20:11:14 -0300

    feat: implementar UserServiceApplication func GetUser no contexto interno user

--- internal/user/application/service.go
+++ internal/user/application/service.go
@@ -1,1 +1,30 @@
 package application
+
+import "github.com/marcelofabianov/pejota/internal/user/port"
+
+type UserServiceApplication struct {
+	getUser port.GetUserUseCase
+}
+
+func NewUserServiceApplication(getUser port.GetUserUseCase) port.UserServiceApplication {
+	return &UserServiceApplication{getUser}
+}
+
+func (s *UserServiceApplication) GetUser(input port.GetUserInputServiceApplication) (port.GetUserOutputServiceApplication, error) {
+	inputUseCase := port.GetUserInputUseCase{GetUserInputServiceApplication: input}
+
+	outputUseCase, err := s.getUser.Execute(inputUseCase)
+	if err != nil {
+		return port.GetUserOutputServiceApplication{}, err
+	}
+
+	return port.GetUserOutputServiceApplication{
+		PublicID:     outputUseCase.PublicID,
+		Name:         outputUseCase.Name,
+		Email:        outputUseCase.Email,
+		Role:         outputUseCase.Role,
+		LoginEnabled: outputUseCase.LoginEnabled,
+		CreatedAt:    outputUseCase.CreatedAt,
+		UpdatedAt:    outputUseCase.UpdatedAt,
+	}, nil
+}
