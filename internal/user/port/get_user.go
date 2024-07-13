3c946d7aece12afd14e885038966f820b1cc5cc8
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-13 20:00:21 -0300

    feat: adicionar interfaces para user repository em ports do contexto interno user

--- internal/user/port/get_user.go
+++ internal/user/port/get_user.go
@@ -1,31 +1,55 @@
 package port
 
+// Service
+
 type GetUserInputService struct {
 	PublicID string `json:"public_id" validate:"required,uuid"`
 }
 
 type GetUserOutputService struct {
-	PublicID     string `json:"public_id"`
-	Name         string `json:"name"`
-	Email        string `json:"email"`
-	Role         string `json:"role"`
-	LoginEnabled bool   `json:"login_enabled"`
-	CreatedAt    string `json:"created_at"`
-	UpdatedAt    string `json:"updated_at"`
+	PublicID     string `json:"public_id" validate:"required,uuid"`
+	Name         string `json:"name" validate:"required,string,min=3,max=255"`
+	Email        string `json:"email" validate:"required,email"`
+	Role         string `json:"role" validate:"required,role"`
+	LoginEnabled bool   `json:"login_enabled" validate:"required,bool"`
+	CreatedAt    string `json:"created_at" validate:"required,datetime"`
+	UpdatedAt    string `json:"updated_at" validate:"required,datetime"`
 }
+
+type GetUserService interface {
+	GetUser(input GetUserInputService) (GetUserOutputService, error)
+}
+
+// UseCase
 
 type GetUserInputUseCase struct {
 	GetUserInputService
 }
 
 type GetUserOutputUseCase struct {
-	GetUserOutputService
+	GetUserOutputRepository
 }
 
 type GetUserUseCase interface {
 	Execute(input GetUserInputUseCase) (GetUserOutputUseCase, error)
 }
 
-type GetUserService interface {
-	GetUser(input GetUserInputService) (GetUserOutputService, error)
+// Repository
+
+type GetUserInputRepository struct {
+	PublicID string
+}
+
+type GetUserOutputRepository struct {
+	PublicID     string
+	Name         string
+	Email        string
+	Role         string
+	LoginEnabled bool
+	CreatedAt    string
+	UpdatedAt    string
+}
+
+type GetUserRepository interface {
+	GetUser(input GetUserInputRepository) (GetUserOutputRepository, error)
 }
