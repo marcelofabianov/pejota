9d283e874aa3d7469fafaea4ad8fbb0be84eb028
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-14 11:49:46 -0300

    feat: atualizar UserContainer para dependencia de Hash para CreateUserUseCase

--- internal/user/container.go
+++ internal/user/container.go
@@ -8,6 +8,7 @@ import (
 	"github.com/marcelofabianov/pejota/internal/user/application"
 	"github.com/marcelofabianov/pejota/internal/user/domain/usecase"
 	"github.com/marcelofabianov/pejota/internal/user/port"
+	"github.com/marcelofabianov/pejota/pkg/hasher"
 )
 
 type UserContainer struct {
@@ -17,11 +18,18 @@ type UserContainer struct {
 func NewUserContainer(db *pgx.Conn) *UserContainer {
 	container := dig.New()
 
+	registerPackages(container)
 	registerRepositories(container, db)
 	registerUseCases(container)
 	registerService(container)
 
 	return &UserContainer{Container: container}
+}
+
+func registerPackages(container *dig.Container) {
+	container.Provide(func() port.PasswordHasher {
+		return hasher.NewHasher()
+	})
 }
 
 func registerRepositories(container *dig.Container, db *pgx.Conn) {
@@ -35,8 +43,8 @@ func registerUseCases(container *dig.Container) {
 		return usecase.NewGetUserUseCase(repo)
 	})
 
-	container.Provide(func(repo port.UserRepository) port.CreateUserUseCase {
-		return usecase.NewCreateUserUseCase(repo)
+	container.Provide(func(hasher port.PasswordHasher, repo port.UserRepository) port.CreateUserUseCase {
+		return usecase.NewCreateUserUseCase(hasher, repo)
 	})
 }
 
